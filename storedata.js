5140d39190db053129a7a256929f000ff19b40e3
siddiquimaha <siddiqui.mahajabeen@gmail.com>
2025-01-04 10:55:33 -0800

    firebase connected

--- storedata.js
+++ storedata.js
@@ -1,4 +1,54 @@
+// script for contact form for firebase connection
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+// import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-database.js";
 
+
+// const firebaseConfig = {
+//     apiKey: "AIzaSyB-QkJ4x4HrJlmV2xoQZ-3YPI7f0kW8-eM",
+//     authDomain: "firsthackathon-6fb25.firebaseapp.com",
+//     projectId: "firsthackathon-6fb25",
+//     storageBucket: "firsthackathon-6fb25.firebasestorage.app",
+//     messagingSenderId: "761124516534",
+//     appId: "1:761124516534:web:e6d7db90c0cd770b662c17"
+//   };
+
+
+// const app = initializeApp(firebaseConfig);        
+// const database = getDatabase(app);
+
+// let form = document.getElementById("contact-form");
+// let modal = document.getElementById("success-modal");
+// let closeModalButton = document.getElementById("close-modal");
+
+// form.addEventListener("submit", function (event) {
+//     event.preventDefault();
+
+//     let name = document.getElementById("name").value;
+//     let email = document.getElementById("email").value; 
+//     let message = document.getElementById("message").value; 
+
+//     console.log("Name:", name);
+    // modal.style.display = "block";
+      // Push data to Firebase
+    // push(ref(database, 'contacts'), {
+    //   name: name,
+    //   email: email,
+    //   message: message,
+    // }).then(() => {
+    //   alert('Message submitted successfully!');
+        // modal.style.display = "block";
+//       contactForm.reset();
+//     }).catch((error) => {
+//       console.error("Error writing to Firebase:", error);
+//     });
+
+// });
+
+closeModalButton.addEventListener("click", function () {
+    modal.style.display = "none";
+});
+
+// end for contact pop up
 document.addEventListener("DOMContentLoaded", () => {
     const cartBadge = document.getElementById("cart-badge");
     const shopNowButtons = document.querySelectorAll(".shop-now");
